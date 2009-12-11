6583b8dca7991cc11280e076393944a2dd2a2038
kimppal <kimppal@localhost>
2009-12-11 12:21:12 +0000

    Skeleton for ConfigUtil

---
+++ src/com/vaadin/incubator/simpleshop/util/ConfigUtil.java
@@ -0,0 +1,15 @@
+package com.vaadin.incubator.simpleshop.util;
+
+/**
+ * Configurations utility class
+ * 
+ * @author Kim
+ * 
+ */
+public class ConfigUtil {
+
+    public static String getValue(String key) {
+        return "";
+    }
+
+}
