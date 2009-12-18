e404c1db45232d118b8389c16260d3e471cf08fe
kimppal <kimppal@localhost>
2009-12-18 14:03:10 +0000

    Added an annotation for setting the translations for pojo fields

---
+++ src/com/vaadin/incubator/simpleshop/annotations/FieldTranslation.java
@@ -0,0 +1,7 @@
+package com.vaadin.incubator.simpleshop.annotations;
+
+import com.vaadin.incubator.simpleshop.lang.SystemMsg;
+
+public @interface FieldTranslation {
+    public SystemMsg name();
+}
